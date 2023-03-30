4a68ab16a118cee5f6c41653eb00fe9214f3829c
aboubacar108 <aboubacar.kaba108@gmail.com>
2023-03-29 21:00:09 -0400

    added User Model, Signup & Login Routes and Custom Errors

--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
+require('dotenv').config();
+require('express-async-errors');
+
 const express = require('express');
 const app = express();
 const cors = require('cors')
 const favicon = require('express-favicon');
 const logger = require('morgan');
 
+// routers
+const authRouter = require('./routes/auth');
+
 const mainRouter = require('./routes/mainRouter.js');
 
 // middleware
-app.use(cors());
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // routes
+app.use('/api/v1/auth', authRouter)
 app.use('/api/v1', mainRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
-const { PORT = 8000 } = process.env;
 const app = require("./app");
+// connectDB
+const connectDB = require('./db/connect');
+//const authenticateUser = require('./middleware/authentication');
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const port = process.env.PORT || 8000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
